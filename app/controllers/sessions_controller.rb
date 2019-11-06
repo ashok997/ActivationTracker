fa12a442efc93084b35b5fb357eea859c80a127d
ashok997 <ashok997@gmail.com>
2019-11-06 14:42:42 -0600

    added login page and route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,15 @@ class SessionsController < ApplicationController
         redirect_to root_path
     end
 
-    def new
+    def create
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message] = "Incorrect login info, please try again!!"
+            redirect_to "/login"
+        end
 
     end
 
