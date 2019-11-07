3c5ee89330ae0dea2014eed3cddc97cc395b9b09
ashok997 <ashok997@gmail.com>
2019-11-06 18:38:53 -0600

    added controller and show page for phone model

--- app/controllers/carriers_controller.rb
+++ app/controllers/carriers_controller.rb
@@ -1,2 +1,18 @@
 class CarriersController < ApplicationController
+    def new
+        @carrier = Carrier.new
+    end
+    
+    def create
+        @phone = Phone.new(phone_params)
+        if @phone.save
+            redirect_to phones_path
+        else
+            render :new
+        end
+    end
+
+    def phone_params
+        params.require(:phone).permit(:model, :manufacturer, :storage )
+    end
 end
--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,2 +1,19 @@
 class PhonesController < ApplicationController
+
+    def new
+        @phone = Phone.new
+    end
+    
+    def create
+        @phone = Phone.new(phone_params)
+        if @phone.save
+            redirect_to phones_path
+        else
+            render :new
+        end
+    end
+
+    def phone_params
+        params.require(:phone).permit(:model, :manufacturer, :storage )
+    end
 end
