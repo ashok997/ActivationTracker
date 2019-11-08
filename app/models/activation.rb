e56fcaff506f73819e34160628670d924a2b18cf
ashok997 <ashok997@gmail.com>
2019-11-08 17:51:04 -0600

    added validation for activation

--- app/models/activation.rb
+++ app/models/activation.rb
@@ -3,6 +3,9 @@ class Activation < ApplicationRecord
   belongs_to :carrier
   belongs_to :phone
 
+  validates :phone_number, :IMEI, presence: true
+  validates :IMEI, :phone_number, uniqueness: true
+
   def self.by_user(user_id)
     where(user: user_id)
   end
